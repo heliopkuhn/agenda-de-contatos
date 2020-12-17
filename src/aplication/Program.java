f834ae475b7df2902aeaace420aaf22e85ea6f40
heliopkuhn <heliopkuhn@gmail.com>
2020-12-16 22:28:21 -0300

    Classe de conexao criada

---
+++ src/aplication/Program.java
@@ -0,0 +1,16 @@
+package aplication;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn = DB.getConnection();
+		
+		DB.closeConection();
+	}
+
+}
